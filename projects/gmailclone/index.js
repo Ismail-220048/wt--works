13821d63d1963139f0ae422aeab46741149ccda6
Ismail-220048 <n220048@rguktn.ac.in>
2026-01-07 22:35:40 +0530

    now all emails are opens

--- projects/gmailclone/index.js
+++ projects/gmailclone/index.js
@@ -1,32 +1,31 @@
-function openTab(event, tabId) {
 
-  for (let content of document.querySelectorAll('.tab-content')) {
-    content.classList.remove('active');
-  }
+    function openTab(event, tabId) {
+      for (let content of document.querySelectorAll('.tab-content')) {
+        content.classList.remove('active');
+      }
+      for (let menu of document.querySelectorAll('.menu div')) {
+        menu.classList.remove('active');
+      }
+      for (let tab of document.querySelectorAll('.tabs .tab')) {
+        tab.classList.remove('active');
+      }
+      document.getElementById(tabId).classList.add('active');
+      event.target.classList.add('active');
+    }
 
-  for (let menu of document.querySelectorAll('.menu div')) {
-    menu.classList.remove('active');
-  }
+    function openMail(mail) {
+      var sender = mail.querySelector(".sender").innerText;
+      var subject = mail.querySelector(".subject").innerText;
+      var time = mail.querySelector(".time").innerText;
 
-  for (let tab of document.querySelectorAll('.tabs .tab')) {
-    tab.classList.remove('active');
-  }
+      document.getElementById("popupSender").innerText = sender;
+      document.getElementById("popupSubject").innerText = subject;
+      document.getElementById("popupTime").innerText = time;
 
-  document.getElementById(tabId).classList.add('active');
-  event.target.classList.add('active');
-}
-function openMail(mail) {
-  var sender = mail.querySelector(".sender").innerText;
-  var subject = mail.querySelector(".subject").innerText;
-  var time = mail.querySelector(".time").innerText;
+      document.getElementById("popup").style.display = "block";
+    }
 
-  document.getElementById("popupSender").innerText = sender;
-  document.getElementById("popupSubject").innerText = subject;
-  document.getElementById("popupTime").innerText = time;
-
-  document.getElementById("popup").style.display = "block";
-}
-
-function cls(){
-  document.getElementById("popup").style.display = "none";
-}
+    
+    function cls() {
+      document.getElementById("popup").style.display = "none";
+    }
